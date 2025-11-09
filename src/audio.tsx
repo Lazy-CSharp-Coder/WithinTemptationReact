9deede728f54c0a6787c3a30a952f64b03ee339a
Lazy-CSharp-Coder <harald.nydal@gmail.com>
2025-11-09 14:10:56 +0100

    ok...får det ikke til helt

--- src/audio.tsx
+++ src/audio.tsx
@@ -1,9 +1,17 @@
-type audioTrack = 
+interface audioTrack 
 {
     trackNumber : Number;
     trackName : String;
     trackMinutes : Number;
     trackSeconds : Number;
-    audioToPlay : ;
+    audioToPlay : HTMLAudioElement;
+};
 
-}
+const playList  =  
+{
+    theUnforgiving : Array<audioTrack> = [ {trackNumber:  0, trackName : "Why Not Me", trackMinutes : 0, trackSeconds : 0, audioToPlay : new Audio("AudioTracks/01 - Why Not Me.mp3") ,} ,
+                        {trackNumber : 1, trackName : "Shot In The Dark", trackMinutes : 0, trackSeconds : 0 , audioToPlay : new Audio("AudioTracks/02 - Shot In The Dark.mp3") } ]
+};
+
+let isPlaying : boolean = false;
+let songPlaying : number = 0;
