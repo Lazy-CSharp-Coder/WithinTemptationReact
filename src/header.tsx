1a8e0f7a8940ff05bb9f8c5084736745463735b3
Harald Nydal <haraldn.kodeloftet@gmail.com>
2025-11-21 17:51:09 +0100

    begynt header

---
+++ src/header.tsx
@@ -0,0 +1,9 @@
+export function header() : React.ReactElement
+{
+    return(
+     <header className="flex">
+      <img class="logoHeader" src="Album/gunlogo2.gif">
+      <img id="hamburgerIcon" class="hamburgerIcon" src="Icons/hamburger.png">
+    </header>
+     )
+}
