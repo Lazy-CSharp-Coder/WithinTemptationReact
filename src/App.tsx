64d82dea7d684bb0c1e73e02a7d65c76e7d1c387
Harald Nydal <haraldn.kodeloftet@gmail.com>
2025-11-22 09:42:09 +0100

    fikset l header nå fixed instead of sticky

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,17 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 // mine moduler som jeg prøver meg på 
-import { Header } from "./modules/header.tsx"
+import { Header } from "./modules/Header.tsx"
 import { BackgroundImage, currentAlbum } from "./modules/BackgroundImage.tsx"
 
 function App() {
-
-
   return (
     <>
+      <BackgroundImage albumNumber={currentAlbum} />
       <Header/>
      <main>
-        <BackgroundImage albumNumber={currentAlbum} />
-      </main>
+      
+     </main>
      
     </>
   );
