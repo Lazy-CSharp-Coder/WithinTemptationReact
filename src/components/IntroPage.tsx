a18fb62b4c8a142b4f9d7c044c73b0279699f567
Harald Nydal <haraldn.kodeloftet@gmail.com>
2026-01-03 19:46:12 +0100

    test

--- src/components/IntroPage.tsx
+++ src/components/IntroPage.tsx
@@ -10,7 +10,7 @@ export function IntroPage({exitPage} : { exitPage : () => void})
                    <p className="introText">Originally, this was a Frontend Practice project with some guy I never heard of and hadn't listened to in the past. I started making it in Vanilla.</p>
                    <p className="introText">So, then I thought : "Why not make this an album page for one of Within Temptations albums since I already have a lot of their CD's (yes, I'm from the CD generation :-). 
                       I've listened to them over the years, since they had a big breakthrough in 2004 with "Stand My Ground" from "The Silent Force".</p>
-                    <p className="introText">I started changing it and then began planning the page to switch between several albums. </p>
+                    <p className="introText">I started changing it and then began planning the page to switch between several albums and listen to them. </p>
                     <button className="exitIntroButton flex" onClick={exitPage}>
                         <img className="logoButtonIntro" src="/icons/wtlogov2.jpg" alt="logo within temptation"/>
                         Enter Page</button>
