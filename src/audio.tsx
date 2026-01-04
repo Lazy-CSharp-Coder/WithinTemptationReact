3244da09ad154c639970514ac0716e5c17b2b125
Harald Nydal <haraldn.kodeloftet@gmail.com>
2026-01-04 17:23:26 +0100

    test

--- src/audio.tsx
+++ src/audio.tsx
@@ -61,33 +61,11 @@ export const albums : record[] =
              { nr : 12,  title : "What Have You Done (Rock Mix)", audio : new Audio("/AudioTracks/theHeart/12 - What Have You Done (Rock Mix).mp3"), time : "0:0"},
             
              ]
-    },
-     {   artistName : "Within Temptation",
-        albumName : "Resist",
-        albumCover : "/images/acoustic.jpg",
-        releaseDate : new Date(2009, 9, 30),
-        backgroundImage : "/images/acousticBackground.jpg",
-        tracks : [ 
-             { nr : 1, title : "The Howling", audio : new Audio("/AudioTracks/theHeart/01 - The Howling.mp3"),  time : "0:0" },
-             { nr : 2, title : "What Have You Done" ,audio :  new Audio("/AudioTracks/theHeart/02 - What Have You Done.mp3"), time : "0:0" },
-             { nr : 3, title : "Frozen", audio :  new Audio("/AudioTracks/theHeart/03 - Frozen.mp3"), time : "0:0"},
-             { nr : 4,  title : "Our Solemn Hour", audio :  new Audio("/AudioTracks/theHeart/04 - Our Solemn Hour.mp3"),  time : "0:0" },
-             { nr : 5,  title : "The Heart Of Everything", audio :  new Audio("/AudioTracks/theHeart/05 - The Heart Of Everything.mp3"),  time : "0:0" },
-             { nr : 6,  title : "Hand Of Sorrow", audio :  new Audio("/AudioTracks/theHeart/06 - Hand Of Sorrow.mp3"), time : "0:0" },
-             { nr : 7,  title : "The Cross", audio :  new Audio("/AudioTracks/theHeart/07 - The Cross.mp3"),    time : "0:0"},
-             { nr : 8,  title : "Final Destination", audio :  new Audio("/AudioTracks/theHeart/08 - Final Destination.mp3"),  time : "0:0"} ,
-             { nr : 9,  title : "All I Need", audio :  new Audio("/AudioTracks/theHeart/09 - All I Need.mp3"),    time : "0:0"},
-             { nr : 10,  title : "The Truth Beneath The Rose", audio : new Audio("/AudioTracks/theHeart/10 - The Truth Beneath The Rose.mp3"),  time : "0:0" },
-             { nr : 11,  title : "Forgiven", audio : new Audio("/AudioTracks/theHeart/11 - Forgiven.mp3"),    time : "0:0" },
-             { nr : 12,  title : "What Have You Done (Rock Mix)", audio : new Audio("/AudioTracks/theHeart/12 - What Have You Done (Rock Mix).mp3"), time : "0:0"},
-            
-             ]
-    },
-     {   artistName : "Within Temptation",
+    },{   artistName : "Within Temptation",
         albumName : "An Acoustic Night at the Theatre",
      albumCover : "/images/acoustic.jpg",
         releaseDate : new Date(2009, 9, 30),
-        backgroundImage : "/images/acousticBackground.jpg",
+        backgroundImage : "/images/acousticBackground.jpg", 
         tracks : [ 
              { nr : 1, title : "Towards The End", audio : new Audio("/AudioTracks/acoustic/01 - Towards The End.mp3"),  time : "0:0" },
              { nr : 2, title : "Stand My Ground" ,audio :  new Audio("/AudioTracks/acoustic/02 - Stand My Ground.mp3"), time : "0:0" },
@@ -103,7 +81,28 @@ export const albums : record[] =
              { nr : 12,  title : "Utopia ( Feat. Chris Jones )", audio : new Audio("/AudioTracks/acoustic/12 - Utopia ( Feat. Chris Jones ).mp3"), time : "0:0"},
             
              ]
-    }
+    },
+     {   artistName : "Within Temptation",
+        albumName : "Resist",
+        albumCover : "/images/resist.jpg",
+        releaseDate : new Date(2009, 9, 30),
+        backgroundImage : "/images/resistBackground.jpg",
+        tracks : [ 
+             { nr : 1, title : "The Reckoning (feat. Jacoby Shaddix)", audio : new Audio("/AudioTracks/resist/01 - The Reckoning (feat. Jacoby Shaddix).mp3"),  time : "0:0" },
+             { nr : 2, title : "Endless War" ,audio :  new Audio("/AudioTracks/resist/t03 - Raise Your Banner (feat. Anders Fridén).mp3"), time : "0:0" },
+             { nr : 3, title : "Raise Your Banner (feat. Anders Fridén)", audio :  new Audio("/AudioTracks/resist/03 - Frozen.mp3"), time : "0:0"},
+             { nr : 4,  title : "Supernova", audio :  new Audio("/AudioTracks/resist/04 - Supernova.mp3"),  time : "0:0" },
+             { nr : 5,  title : "Holy Ground", audio :  new Audio("/AudioTracks/resist/05 - Holy Ground.mp3"),  time : "0:0" },
+             { nr : 6,  title : "In Vain", audio :  new Audio("/AudioTracks/resist/06 - In Vain.mp3"), time : "0:0" },
+             { nr : 7,  title : "Firelight (feat. Jasper Steverlinck)", audio :  new Audio("/AudioTracks/resist/07 - Firelight (feat. Jasper Steverlinck).mp3"),    time : "0:0"},
+             { nr : 8,  title : "Mad World", audio :  new Audio("/AudioTracks/resist/08 - Mad World.mp3"),  time : "0:0"} ,
+             { nr : 9,  title : "Mercy Mirror", audio :  new Audio("/AudioTracks/resist/09 - Mercy Mirror.mp3"),    time : "0:0"},
+             { nr : 10,  title : "Trophy Hunter", audio : new Audio("/AudioTracks/resist/10 - Trophy Hunter Rose.mp3"),  time : "0:0" },
+           
+            
+             ]
+    },
+     
 ];
 
 
